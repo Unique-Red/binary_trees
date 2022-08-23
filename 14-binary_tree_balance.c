854b3157687ed18580a410d9bd0a5ede420eb4f5
Unique-Red <vnoah410@gmail.com>
2022-08-23 12:35:06 -0700

    update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,8 +16,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	left = custom_binary_tree_height(tree->left);
-	right = custom_binary_tree_height(tree->right);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 	return (left - right);
 }
 
